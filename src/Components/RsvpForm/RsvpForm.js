2eca242586b3f35355191c3e9f055ba4f10e48eb
Vendy Prum <vendy@thinkful.com>
2020-11-12 16:24:34 -0800

    post request for attendees

--- src/Components/RsvpForm/RsvpForm.js
+++ src/Components/RsvpForm/RsvpForm.js
@@ -1,11 +1,38 @@
 import React from "react";
-
+import Config from '../../Config'
+import './RsvpForm.css'
 export default class RsvpForm extends React.Component {
   state = {
     firstname: "",
-    lastname: ""
+    lastname: "",
+    error:null,
+    success:false,
+    data:[]
   };
 
+  onSubmitGuestName=(e)=>{
+    e.preventDefault();
+    fetch(`${Config.API_ENDPOINT}/rsvp`,{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify({
+          firstname:this.state.firstname,
+          lastname:this.state.lastname
+      })
+    })
+      .then((res)=>{
+        if(!res.ok){
+          throw new Error(res.statusText);
+        }
+        return res.json()
+      })
+      .then(res =>{
+        this.setState({success:true, data:res})
+      })
+      .catch((err)=>err.message)
+  }
   updateFirstname = (value) => {
     this.setState({ firstname: value });
   };
@@ -13,9 +40,15 @@ export default class RsvpForm extends React.Component {
     this.setState({ lastname: value });
   };
   render() {
+    console.log(this.state.data)
+    const status = <div><label htmlFor="status">Attending? (Y/N)</label><input type="checkbox" id="status"/></div> 
+    console.log(status.checked)
+    const people = this.state.data.map((user,id)=>{
+    return <li className="people" key={id}>{user.firstname} {user.lastname} {status}</li>
+    })
     return (
       <div>
-        <form>
+        {this.state.success?people:<form onSubmit={e=>this.onSubmitGuestName(e)}>
           <label htmlFor="firstname">Firstname:</label>
           <input
             id="firstname"
@@ -31,7 +64,7 @@ export default class RsvpForm extends React.Component {
             required
           />
           <button>Submit</button>
-        </form>
+        </form>}
       </div>
     );
   }
